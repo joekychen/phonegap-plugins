c8f2686827a1f49f34e68f1280949624df966b8b
joekychen <chensi@healskare.cm>
2012-07-10 16:44:10 +0800

    Update master

--- Android/Analytics/src/com/phonegap/plugins/analytics/GoogleAnalyticsTracker.java
+++ Android/Analytics/src/com/phonegap/plugins/analytics/GoogleAnalyticsTracker.java
@@ -1,9 +1,4 @@
-/*
- * PhoneGap is available under *either* the terms of the modified BSD license *or* the
- * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
- *
- * Copyright (c) 2006-2011 Worklight, Ltd.  
- */
+#    Google Analytics Tracker
 
 package com.phonegap.plugins.analytics;
 
